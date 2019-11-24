7929597119e207d602ed839332f3cbad2c98190c
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2019-11-23 21:30:24 -0500

    the guy moves

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
+let debug = false;
+let game = true;
+
 
 var Enemy = function(x,y) {
     this.x = x;
     this.y = y;
-
+    this.height = 65;
+    this.width = 95;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -23,7 +27,8 @@ Enemy.prototype.render = function() {
 var Player = function(x,y,sprite) {
     this.x = x;
     this.y = y;
-
+    this.height = 75;
+    this.width = 65;
     this.sprite = sprite;
 };
 
@@ -33,6 +38,18 @@ Player.prototype.update = function(dt) {
 
 };
 
+
+Player.prototype.handleInput = function(direction) {
+    if ( direction === 'left') {
+        this.x -= 100;
+    } else if ( direction === 'right') {
+        this.x += 100;
+    } else if ( direction === 'up') {
+        this.y -= 83;
+    } else if ( direction === 'down') {
+        this.y += 83;
+    }
+};
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -52,5 +69,10 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
+
+const enemyPosition = [55,140,230];
+
 const player = new Player(202, 400, 'images/char-boy.png');
-const allEnemies = [];
+const allEnemies = enemyPosition.map((y, index) =>{
+    return new Enemy( (-200 * (index + 1)), y );
+});
